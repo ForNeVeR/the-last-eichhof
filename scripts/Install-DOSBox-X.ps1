1e66e8025da987e3174f158e32aa923b86d179f8
Friedrich von Never <friedrich@fornever.me>
2026-01-23 01:13:02 +0100

    CI: update the installation script

--- scripts/Install-DOSBox-X.ps1
+++ scripts/Install-DOSBox-X.ps1
@@ -9,21 +9,25 @@ if ($IsLinux)
 {
     Write-Host 'Installing DOSBox-X on Linux using APT…'
     sudo apt-get update
+    if (!$?) { throw "Error from apt-get: $LASTEXITCODE." }
     sudo apt-get install -y dosbox-x
+    if (!$?) { throw "Error from apt-get: $LASTEXITCODE." }
 }
 elseif ($IsMacOS)
 {
     Write-Host 'Installing DOSBox-X on macOS using Homebrew…'
     brew install dosbox-x
+    if (!$?) { throw "Error from brew: $LASTEXITCODE." }
 }
 elseif ($IsWindows)
 {
-    Write-Host 'Installing DOSBox-X on Windows using WinGet...'
+    Write-Host 'Installing DOSBox-X on Windows using WinGet…'
     winget install --id joncampbell123.DOSBox-X -e
+    if (!$?) { throw "Error from winget: $LASTEXITCODE." }
 }
 else
 {
-    throw 'Unsupported platform'
+    throw 'Unsupported platform.'
 }
 
 Write-Host 'DOSBox-X installation completed successfully.'
